e98f8bdbd03f117912c75a588065712e72484701
BUPT-HJM <452114240@qq.com>
2017-07-07 10:54:22 +0800

    fix bubbleSort

--- 排序算法/冒泡排序.js
+++ 排序算法/冒泡排序.js
@@ -1,6 +1,6 @@
 function bubbleSort(arr) {
   for (var i = 0, length = arr.length; i < length; i++) {
-    for (var j = 0; j < length - i; j++) {
+    for (var j = 0; j < length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         var temp = arr[j + 1];
         arr[j + 1] = arr[j];
